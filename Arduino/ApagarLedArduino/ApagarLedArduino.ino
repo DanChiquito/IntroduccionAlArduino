607685c1d1fff6a49879dd754b9a808ada0f981b
DanChiquito <43891363+DanChiquito@users.noreply.github.com>
2018-11-07 14:33:54 -0600

    Add files via upload

---
+++ Arduino/ApagarLedArduino/ApagarLedArduino.ino
@@ -0,0 +1,16 @@
+
+//Declarar varibles para uso de los ´puertos
+
+int LED =13; //El NUMERO SE ASOCIA CON EL PUERTO
+
+void setup() {
+  pinMode(LED, OUTPUT); //pinMode(puerto , entrada o salida)
+
+}
+
+void loop() {
+digitalWrite(LED, HIGH); // digitalWrite(puerto, HIGH o LOW) HIGH entrega 5 volts y LOW nada
+delay(1000); // Esta en milisegundos
+digitalWrite(LED, LOW);
+delay(1000);
+}
